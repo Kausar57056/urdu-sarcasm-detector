2ac3ab47f9a089e4039b2c94ed1d89dc5be9380b
Kausar57056 <163397471+Kausar57056@users.noreply.github.com>
2025-07-04 12:26:26 +0500

    Update app.py

--- app.py
+++ app.py
@@ -11,7 +11,7 @@ from transformers import AutoTokenizer, XLMRobertaModel
 class SentimixtureNet(nn.Module):
     def __init__(self):
         super(SentimixtureNet, self).__init__()
-        self.base = XLMRobertaModel.from_pretrained("tokenizer/")
+        self.base = XLMRobertaModel.from_pretrained("xlm-roberta-base")
         self.routing = nn.Linear(768, 768)
         self.attn = nn.MultiheadAttention(embed_dim=768, num_heads=8, batch_first=True)
         self.classifier = nn.Linear(768, 2)
