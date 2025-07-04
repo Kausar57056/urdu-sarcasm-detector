5c62b5307fd95f58291b0583e9233f7a13a83572
Kausar57056 <163397471+Kausar57056@users.noreply.github.com>
2025-07-05 02:04:09 +0500

    Update app.py

--- app.py
+++ app.py
@@ -147,11 +147,24 @@ cols = st.columns(len(examples))
 for i, example in enumerate(examples):
     cols[i].button(example, key=f"ex{i}", on_click=set_example, args=(example,))
 
-
 # Centered Detect Button
 col1, col2, col3 = st.columns([1, 2, 1])
 with col2:
     detect = st.button("🔎 Detect Sarcasm")
+
+st.markdown(
+    """
+    <style>
+    div.stButton > button {
+        margin-left: auto;
+        margin-right: auto;
+        display: block;
+        width: 180px;  /* or any width you want */
+    }
+    </style>
+    """,
+    unsafe_allow_html=True,
+)
 
 # Load model
 try:
